a81840c78dccbcabe3f1c9db31396a1151d22208
hasibulHossain <51342498+hasibulHossain@users.noreply.github.com>
2021-01-06 18:32:11 +0600

    Create array.js

---
+++ array.js
@@ -0,0 +1,136 @@
+const paradox = [
+  {
+    name: "ahsan",
+    children: [
+      {
+        name: "rashid",
+        children: [
+          {
+            name: "ruma",
+            children: [
+              {
+                name: "rafsan",
+                children: [],
+              },
+            ],
+          },
+          {
+            name: "nazma",
+            children: [],
+          },
+          {
+            name: "raesl",
+            children: [
+              {
+                name: "rose",
+                children: [],
+              },
+            ],
+          },
+          {
+            name: "panna",
+            children: [
+              {
+                name: "payel",
+                children: [],
+              },
+            ],
+          },
+          {
+            name: "sinin",
+            children: [],
+          },
+        ],
+      },
+      {
+        name: "kabir",
+        children: [
+          {
+            name: "rabbi",
+            children: [],
+          },
+          {
+            name: "raisa",
+            children: [],
+          },
+        ],
+      },
+      {
+        name: "kalam",
+        children: [
+          {
+            name: "yamin",
+            children: [],
+          },
+          {
+            name: "aymon",
+            children: [
+              {
+                name: "john",
+                children: [
+                  {
+                    name: "clency",
+                    children: [
+                      {
+                        name: "jacky",
+                        children: [],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "kumkum",
+            children: [],
+          },
+        ],
+      },
+      {
+        name: "faruque",
+        children: [
+          {
+            name: "joba",
+            children: [],
+          },
+          {
+            name: "farhan",
+            children: [],
+          },
+          {
+            name: "mymuna",
+            children: [],
+          },
+        ],
+      },
+      {
+        name: "akbar",
+        children: [
+          {
+            name: "tanha",
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "shumon mia",
+    children: [
+      {
+        name: "samsu",
+        children: [
+          {
+            name: "shakil",
+            children: [],
+          },
+        ],
+      },
+      {
+        name: "sonia",
+        children: [],
+      },
+    ],
+  },
+];
